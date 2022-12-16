e8893f2e6ee3068ff256a0c0b6b41cbcaa92e124
asakuno <takahiro.asakuno@gmail.com>
2022-12-16 16:59:23 +0900

    [Add]views

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,10 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :email, presence: true
   validates :name, presence: true
+
+  def own?(object)
+    # 呼び出し元のオブジェクトのIDを示す self.id を省略した記法。
+    # @user.mine?(object)のように利用すると、object.user_id と @user.id を比較する。
+    object.user_id == id
+  end
 end
