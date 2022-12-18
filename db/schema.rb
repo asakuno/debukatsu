69c8cc1adbd53419d53b0336892e30a6980c00a0
asakuno <takahiro.asakuno@gmail.com>
2022-12-18 16:55:35 +0900

    [Add]group_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_13_032731) do
+ActiveRecord::Schema.define(version: 2022_12_18_075210) do
 
   create_table "foods", charset: "utf8mb4", force: :cascade do |t|
     t.string "food_name", null: false
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2022_12_13_032731) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_foods_on_user_id"
+  end
+
+  create_table "groups", charset: "utf8mb4", force: :cascade do |t|
+    t.string "group_name", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
@@ -32,4 +40,5 @@ ActiveRecord::Schema.define(version: 2022_12_13_032731) do
   end
 
   add_foreign_key "foods", "users"
+  add_foreign_key "groups", "users"
 end
