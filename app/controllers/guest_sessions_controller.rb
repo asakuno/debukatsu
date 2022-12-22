5e5b211eff5b4de44d7aa4f0d46c848ea80cc682
asakuno <takahiro.asakuno@gmail.com>
2022-12-22 11:37:23 +0900

    [Add]guest_login機能

---
+++ app/controllers/guest_sessions_controller.rb
@@ -0,0 +1,13 @@
+class GuestSessionsController < ApplicationController
+  skip_before_action :require_login, only: %i[create]
+
+  def create
+    if current_user 
+      redirect_to root_path, alert: 'すでにログインしています'   
+    else
+      @guest_user = User.guest_user_generate
+      auto_login(@guest_user)
+      redirect_to groups_path, notice: t('created')
+    end
+  end
+end
