70456a6f3a6bef5f5be239ad475909797fea6bf5
asakuno <takahiro.asakuno@gmail.com>
2022-12-27 02:48:25 +0900

    [Remove]controller_to_model

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,5 +4,42 @@ class Group < ApplicationRecord
   has_many :foods, through: :select_foods
 
   validates :group_name, presence: true
-  validates :maximum_amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 100, less_than_or_equal_to: 100000}
+  validates :maximum_amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100_000 }
+
+  # 長すぎるとrubocopに指摘された[<17, 61, 10> 64.11/25]
+  def high_calorie(maximum_amount, food_ids)
+    quantity = food_ids.size
+    price_calorie = Array.new(quantity) { [] }
+
+    food_ids.each_with_index do |id, i|
+      price_calorie[i] << Food.find(id).price
+      price_calorie[i] << Food.find(id).calorie
+    end
+
+    # if文で分岐させたほうがいいかも？
+    dp = Array.new(quantity + 1) { Array.new(maximum_amount + 1, 0) }
+    selection = Array.new(quantity + 1) { Array.new(maximum_amount + 1, '') }
+
+    price_calorie.each_with_index do |(price, calorie), i|
+      (0..maximum_amount).each do |j|
+        # 商品i+1を選ばない場合
+        dp[i + 1][j] = dp[i][j]
+        selection[i + 1][j] = "#{selection[i][j]}0"
+        next unless j - price >= 0 && (dp[i + 1][j] < dp[i][j - price] + calorie)
+
+        # 商品i+1を選ぶ場合
+        dp[i + 1][j] = dp[i][j - price] + calorie
+        selection[i + 1][j] = "#{selection[i][j - price]}1"
+      end
+    end
+
+    @best_foods = []
+
+    (0..quantity - 1).each do |i|
+      if selection[-1][-1].split('')[i] == '1'
+        @best_foods << food_ids[i]
+      end
+    end
+    self.food_ids = @best_foods
+  end
 end
