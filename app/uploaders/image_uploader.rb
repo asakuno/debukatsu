be9e22d0c8c3478599929c5738e657f3b037bf3b
asakuno <takahiro.asakuno@gmail.com>
2022-12-30 23:20:45 +0900

    [Add]画像リサイズ・バリデーションの記載

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,3 +1,27 @@
 class ImageUploader < Shrine
-  # plugins and uploading logic
+  require 'image_processing/mini_magick'
+
+  plugin :processing
+  plugin :versions
+  plugin :delete_raw
+  plugin :validation_helpers
+
+  process(:store) do |io, _context|
+    versions = { original: io }
+
+    io.download do |original|
+      pipeline = ImageProcessing::MiniMagick.source(original)
+
+      versions[:large]  = pipeline.resize_to_limit!(800, 800)
+      versions[:medium] = pipeline.resize_to_limit!(500, 500)
+      versions[:small]  = pipeline.resize_to_limit!(300, 300)
+    end
+
+    versions
+  end
+
+  Attacher.validate do
+    validate_max_size 5 * 1024 * 1024, message: '5MBを超える画像はアップロードできません。'
+    validate_mime_type_inclusion %w[image/jpeg image/png]
+  end
 end
