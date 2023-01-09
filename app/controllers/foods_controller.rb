6b55e416dc98be8f9f61bda4ac9ef0eaf95e6853
asakuno <takahiro.asakuno@gmail.com>
2023-01-09 22:22:18 +0900

    [Add]暫定的なtagのviewとcontrooler

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,11 @@
 class FoodsController < ApplicationController
   def index
     @foods = Food.includes(%i[user groups]).references(:all).order(created_at: :desc).page(params[:page])
+
+    @tags = Food.tag_counts_on(:tags).order('count DESC')
+    if @tag = params[:tag]
+      @foods = Food.tagged_with(params[:tag]).includes(%i[user groups]).references(:all).order(created_at: :desc).page(params[:page])
+    end
   end
 
   def show
@@ -31,6 +36,6 @@ class FoodsController < ApplicationController
   private
 
   def food_params
-    params.require(:food).permit(:food_name, :calorie, :price, :image)
+    params.require(:food).permit(:food_name, :calorie, :price, :image, :tag_list)
   end
 end
