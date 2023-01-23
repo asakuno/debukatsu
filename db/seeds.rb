587aa2ae91fcfcaf6ee11588b1eacf887ca450d0
asakuno <takahiro.asakuno@gmail.com>
2023-01-23 23:15:59 +0900

    [Add]vue-multiselect

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+require "open-uri"
+
+user = User.find_or_create_by(email: 'admin@example.com') do |u|
+  u.name = 'Admin'
+  u.password = 'password'
+  u.password_confirmation = 'password'
+end
+
+CSV.foreach('db/test.csv', headers: true) do |row|
+  Food.create!(
+    food_name: row['food_name'],
+    image: URI.open(row['Image']),
+    price: row['Price'],
+    calorie: row['calorie'],
+    user_id: user.id
+  )
+end
