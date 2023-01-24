56b02c899d15c5e5839ec01fde21f7a4eb2cee58
asakuno <takahiro.asakuno@gmail.com>
2023-01-24 12:50:05 +0900

    [Fix]signupのcssの修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
 module.exports = {
+  important: true,
   mode: 'jit',
-  purge: [
+  purge: {
+    content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js',
-  ],
+    "./pages/**/*.tsx", "./components/**/*.tsx",
+  ]},
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      backgroundImage: (theme) => ({
+        check: "url('/icons/check.svg')",
+        landscape: "url('/images/landscape/2.jpg')",
+      }),
       colors: {
         accent: '#ff00aa',
         original: '#ffcc66'
@@ -16,7 +23,15 @@ module.exports = {
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ["checked"],
+      borderColor: ["checked"],
+      inset: ["checked"],
+      zIndex: ["hover", "active"],
+    },
   },
   plugins: [require("daisyui")],
+  future: {
+    purgeLayersByDefault: true,
+  },
 }
