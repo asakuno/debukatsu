e3bcabf33cf96b363684b37a1706d5fbd874e517
asakuno <takahiro.asakuno@gmail.com>
2023-02-01 22:11:21 +0900

    [Fix]byebugの削除

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -32,10 +32,8 @@ class GroupsController < ApplicationController
         food.group_id = @group.id
       end
     end
-    byebug
 
     if @group.save
-      byebug
       redirect_to groups_path
     else
       render :new
