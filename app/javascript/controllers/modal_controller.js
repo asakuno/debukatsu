ccf11132c27c4cbfe2b55a6110a1e221b7c7353b
asakuno <takahiro.asakuno@gmail.com>
2023-02-11 23:26:26 +0900

    [Add]stimulus_modalのimageとfood_nameの表示

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,13 +1,20 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ['modal', "background"];
+  static targets = ['modal', "background", "imagePreview", "foodName"];
+
+  showPreview(params) {
+    this.imagePreviewTarget.src = params.source;
+    this.foodNameTarget.textContent = params.name;
+  }
 
   handleOpen(event) {
     event.preventDefault();
     this.modalTarget.classList.remove("hidden");
     this.backgroundTarget.classList.remove("opacity-0");
     this.backgroundTarget.classList.add("opacity-100");
+    this.showPreview(event.params);
+    console.log(event.params)
   }
 
   handleClose(event) {
