47bcd66b1bc754c079f29401d1bded9f775543e2
asakuno <takahiro.asakuno@gmail.com>
2023-02-13 23:25:58 +0900

    [Add]checkboxのチェックの保存

--- app/javascript/controllers/checkbox_controller.js
+++ app/javascript/controllers/checkbox_controller.js
@@ -3,5 +3,20 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="checkbox"
 export default class extends Controller {
   connect() {
+    const checkboxes = this.element.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox) => {
+      checkbox.addEventListener('click', this.saveState.bind(this));
+      this.restoreState(checkbox);
+    });
+  }
+
+  saveState(event) {
+    const checkbox = event.currentTarget;
+    localStorage.setItem(checkbox.id, checkbox.checked);
+  }
+
+  restoreState(checkbox) {
+    const checked = localStorage.getItem(checkbox.id) === 'true';
+    checkbox.checked = checked
   }
 }
