2aac302dfb17ecb24e9d5688d22dd18dad25c01d
asakuno <takahiro.asakuno@gmail.com>
2023-02-17 23:25:02 +0900

    [Fix]shrineのs3noダイレクトアップロードを元のコードに変更

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,21 +1,11 @@
+require 'shrine'
 require 'shrine/storage/file_system'
-require 'shrine/storage/s3'
- 
-# アップロードするディレクトリの指定
 
-  s3_options = {
-      access_key_id:     'AKIAYO2Q2AFXMXP6WDXS',
-      secret_access_key: '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',
-      region:            'ap-northeast-1',
-      bucket:            'debukatsu',
-  }
- 
-  Shrine.storages = {
-      cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options),
-      store: Shrine::Storage::S3.new(prefix: 'store', **s3_options),
-  }
- 
-# 使用するプラグインの宣言
+Shrine.storages = {
+  cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'),
+  store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads')
+}
+
 Shrine.plugin :activerecord
-Shrine.plugin :cached_attachment_data # for forms
+Shrine.plugin :cached_attachment_data
 Shrine.plugin :restore_cached_data
