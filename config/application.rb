454843674993286d1ff52a9ad7442c4e304a420b
asakuno <takahiro.asakuno@gmail.com>
2023-02-24 23:31:58 +0900

    [Fix]credentials

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,8 @@ module Debukatsu
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
+    
+
     config.i18n.default_locale = :ja
 
     config.generators.system_tests = nil
