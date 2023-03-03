cfe6930ea0fbaf8109c69dcbd3325251faada039
asakuno <takahiro.asakuno@gmail.com>
2023-03-03 23:28:52 +0900

    [Add]like_model_associate

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
 
   has_many :foods, dependent: :destroy
   has_many :groups, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :likes_foods, through: :likes, source: :food
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
