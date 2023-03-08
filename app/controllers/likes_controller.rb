a85d27492fe93dee092f532904a92631d622bd98
asakuno <takahiro.asakuno@gmail.com>
2023-03-08 23:27:39 +0900

    [Fix]Ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,11 @@
 class LikesController < ApplicationController
   def create
-    food = Food.find(params[:food_id])
-    current_user.like(food)
-    redirect_back fallback_location: foods_path
+    @food = Food.find(params[:food_id])
+    current_user.like(@food)
   end
 
   def destroy
-    food = current_user.likes_foods.find(params[:food_id])
-    current_user.unlike(food)
-    redirect_back fallback_location: foods_path
+    @food = current_user.likes_foods.find(params[:food_id])
+    current_user.unlike(@food)
   end
 end
