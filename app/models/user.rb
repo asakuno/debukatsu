73e1a2a1993edcc3b8da9bedc356ea0e44bb33d9
asakuno <takahiro.asakuno@gmail.com>
2023-04-04 23:47:17 +0900

    [Add]profile_association

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :likes_foods, through: :likes, source: :food
   has_many :comments, dependent: :destroy
+  has_one :profile
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
