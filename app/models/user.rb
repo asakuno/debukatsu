971ffc7d24a2e374777e04089b897ab03cf865a6
asakuno <takahiro.asakuno@gmail.com>
2023-04-06 11:12:20 +0900

    [Add]profile_i18nを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
 
   enum role: { general: 0, guest: 1, admin: 2 }
   enum gender: { female: 0, male: 1, unanswered: 2 }
-  enum age: { child: 0, school_child: 1, teenager: 2, young_adult: 3, middle_adulthood: 4, young_middle_age_adult: 5, young_old: 6 }
+  enum age: { school_child: 0, teenager: 1, young_adult: 2, middle_adulthood: 3, young_old: 4 }
   enum weight: { low: 0, midle: 1, high: 2 }
 
   def own?(object)
