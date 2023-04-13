470ca895f3870871565bf421a207aac7774958f5
asakuno <takahiro.asakuno@gmail.com>
2023-04-14 08:41:25 +0900

    [Add]総calorieの表示

--- app/controllers/api/mine/groups_controller.rb
+++ app/controllers/api/mine/groups_controller.rb
@@ -1,6 +1,5 @@
 class Api::Mine::GroupsController < ApplicationController
   def index
     @groups = current_user.groups
-    #render json: @groups.to_json(only: [:id, :group_name, :created_at])
   end
 end
--- app/controllers/mypage/calendars_controller.rb
+++ app/controllers/mypage/calendars_controller.rb
@@ -1,4 +1,5 @@
 class Mypage::CalendarsController < Mypage::BaseController
   def show
+    @weak_total_calories = current_user.groups.where(created_at: Time.zone.now.beginning_of_week..Time.zone.now.end_of_week).joins(:foods).sum(:calorie)
   end
 end
