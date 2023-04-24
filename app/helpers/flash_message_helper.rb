f0f9aeca0aed1b2d3756dbbbb5756f249054d46e
asakuno <takahiro.asakuno@gmail.com>
2023-04-24 12:47:45 +0900

    [Fix]rubocop

--- app/helpers/flash_message_helper.rb
+++ app/helpers/flash_message_helper.rb
@@ -1,19 +1,19 @@
 module FlashMessageHelper
   def flash_class(level)
     case level
-      when 'notice' then 'bg-blue-100 border-t-4 border-blue-500 text-blue-900'
-      when 'success' then 'bg-green-100 border-t-4 border-green-400 text-green-700'
-      when 'danger' then 'bg-red-100 border-t-4 border-red-400 text-red-700'
-      when 'alert' then 'bg-yellow-100 border-t-4 border-yellow-500 text-yellow-700'
+    when 'notice' then 'bg-blue-100 border-t-4 border-blue-500 text-blue-900'
+    when 'success' then 'bg-green-100 border-t-4 border-green-400 text-green-700'
+    when 'danger' then 'bg-red-100 border-t-4 border-red-400 text-red-700'
+    when 'alert' then 'bg-yellow-100 border-t-4 border-yellow-500 text-yellow-700'
     end
   end
 
   def flash_icon(img)
     case img
-      when 'notice' then content_tag :i, '', class: 'fa-light fa-circle-info', style: 'color: #293f66;'
-      when 'success' then content_tag :i, '', class: 'far fa-check-circle fa-lg', style: 'color: #31b441;'
-      when 'danger' then content_tag :i, '', class: 'far fa-times-circle fa-lg', style: 'color: #e60000;'
-      when 'alert' then content_tag :i, '', class: 'fal fa-exclamation fa-lg', style: 'color: #f78708;'
+    when 'notice' then content_tag :i, '', class: 'fa-light fa-circle-info', style: 'color: #293f66;'
+    when 'success' then content_tag :i, '', class: 'far fa-check-circle fa-lg', style: 'color: #31b441;'
+    when 'danger' then content_tag :i, '', class: 'far fa-times-circle fa-lg', style: 'color: #e60000;'
+    when 'alert' then content_tag :i, '', class: 'fal fa-exclamation fa-lg', style: 'color: #f78708;'
     end
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -6,7 +6,7 @@ class Group < ApplicationRecord
 
   validates :group_name, presence: true
   validates :maximum_amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 30000 }
-  validates :publish, inclusion:{in: [true, false]}
+  validates :publish, inclusion: { in: [true, false] }
 
   # 長すぎるとrubocopに指摘された[<17, 61, 10> 64.11/25]
   def high_calorie(maximum_amount, food_ids)
