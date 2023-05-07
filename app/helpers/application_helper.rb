d62dad671b9cd064e6023f6b3c2e33b00d5f0967
asakuno <takahiro.asakuno@gmail.com>
2023-05-07 21:09:36 +0900

    [Add]meta-tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,30 @@ module ApplicationHelper
 
     page_title.empty? ? base_title : page_title + ' - ' + base_title
   end
+
+  def default_meta_tags
+    {
+      site: 'debukatsu',
+      title: 'デブかつApp',
+      reverse: true,
+      separator: '|',
+      description: 'デブかつAppは忙しい社会人やお金のない学生に向けた太るためのサービスです。',
+      keywords: '太る, カロリー, 社会人, 学生',
+      canonical: 'https://www.debukatsu.net/',
+      noindex: !Rails.env.production?,
+      icon: image_url('favicon.ico'),
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: 'https://www.debukatsu.net/',
+        image: image_url('site_logo.jpg'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+      }
+    }
+  end
 end
