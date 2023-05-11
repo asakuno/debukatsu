4d6dc7aecbf65d9c9fa01dcc763cabc7c09f78de
asakuno <takahiro.asakuno@gmail.com>
2023-05-12 08:35:36 +0900

    [Add]groups_factory

--- spec/system/groups_spec.rb
+++ spec/system/groups_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Groups", type: :system do
-  before do
-    driven_by(:rack_test)
+  describe 'FactoryBot' do
+    it 'creates a valid group' do
+      10.times do
+        FactoryBot.create(:food)
+      end
+      group = FactoryBot.create(:group)
+      
+      expect(group).to be_valid
+      expect(group.group_name).to be_present
+      expect(group.maximum_amount).to be_present
+      expect(group.publish).to be_in([true, false])
+      expect(group.user).to be_present
+    end
   end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
 end
